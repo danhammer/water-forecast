b7f70e564859cdda7cba9f101f6e25363d2327cd
Dan Hammer <dan.s.hammer@gmail.com>
2013-01-13 16:26:36 -0800

    refactored covariate script

--- src/core.R
+++ src/core.R
@@ -1,1 +1,12 @@
 source("covariate.R")
+
+data <- read.dta("../data/restat_data.dta")
+
+## national time index and crisis dummies
+T <- max(data[["time"]])
+crisis.mat <- data[1:T, c("crisis1", "crisis2", "crisis3")]
+
+## example income measure
+inc <- data[1:T, c("income")]
+
+
--- src/covariate.R
+++ src/covariate.R
@@ -1,15 +1,6 @@
 library(foreign)
 library(stats)
 
-data <- read.dta("../data/restat_data.dta")
-
-## national time index and crisis dummies
-T <- max(data[["time"]])
-crisis.mat <- data[1:T, c("crisis1", "crisis2", "crisis3")]
-
-## example income measure
-inc <- data[1:T, c("income")]
-
 body <- function(string) {
   ## returns the body of the supplied string, before the period
   x <- strsplit(string, "\\.")
@@ -23,34 +14,26 @@ idx <- function(string) {
   return(as.numeric(x[[1]][2]))
 }
 
-time.covariate <- function(time.opt = "linear.trend") {
+time.covariate <- function(time.var, opt = "linear.trend") {
   ## accepts an option to specify the method to account for temporal
   ## heterogeneity, and returns the covariate matrix that will be used
   ## in the forecasting regression.
 
-  ## create time index, which is common for each state
-  t <- 1:T
-  
   ## return the time indicator for a linear trend
-  if (time.opt == "linear.trend") {
-    return(cbind(t, crisis.mat))
+  if (opt == "linear.trend") {
+    return(cbind(time.var, crisis.mat))
   }
 
   ## return the log of the time index for a logarithmic trend
-  else if (time.opt == "log.trend") {
-    return(cbind(log(t), crisis.mat))
+  else if (opt == "log.trend") {
+    return(cbind(log(time.var), crisis.mat))
   }
 
   ## return a matrix of dummies, indicating each time index for year
   ## fixed effects, noting that there are no repeated years for each
   ## state (e.g., not monthly observation)
-  else if (time.opt == "year.fe") {
-    return(diag(length(t)))
-  }
-
-  ## return nothing if the option is invalid
-  else {
-    return()
+  else if (opt == "year.fe") {
+    return(diag(length(time.var)))
   }
 }
 
